811355831f0d0527a0235c813f89ee10e3ec24f9
Josh Nothum <joshnothum@gmail.com>
2017-11-22 12:58:15 -0600

    changing dialog box

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -53,39 +53,6 @@ myApp.config(function ($routeProvider, $locationProvider, $mdThemingProvider) {
         }
       }
     })
-}).controller('AppCtrl', function ($scope, $mdDialog, $interval) {
-    
-
-    
-
-    $scope.showAdvanced = function (ev) {
-      $mdDialog.show({
-        controller: DialogController,
-        templateUrl: 'dialog1.tmpl.html',
-        parent: angular.element(document.body),
-        targetEvent: ev,
-        clickOutsideToClose: true
-      })
-        .then(function (answer) {
-          $scope.status = 'You said the information was "' + answer + '".';
-        }, function () {
-          $scope.status = 'You cancelled the dialog.';
-        });
-    };
-
-    function DialogController($scope, $mdDialog) {
-      $scope.hide = function () {
-        $mdDialog.hide();
-      };
-
-      $scope.cancel = function () {
-        $mdDialog.cancel();
-      };
-
-      $scope.answer = function (answer) {
-        $mdDialog.hide(answer);
-      };
-    }
-  });
+})
 
 
