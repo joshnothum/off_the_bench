353563a23ab5dbedc620bbb4c4842eb528dea693
Josh Nothum <joshnothum@gmail.com>
2017-11-29 17:30:24 -0600

    have dropdown and location form ready

--- server/public/scripts/controllers/places.controller.js
+++ server/public/scripts/controllers/places.controller.js
@@ -66,5 +66,20 @@ myApp.controller('PlacesController', function ($http, $location, UserService, Ga
         
         GameService.pushLocation(place);
     };
-    
+   
+    $scope.max = 5;
+    $scope.isReadonly = false;
+
+    $scope.hoveringOver = function (value) {
+        $scope.overStar = value;
+        $scope.percent = 100 * (value / $scope.max);
+    };
+
+    $scope.ratingStates = [
+        { stateOn: 'glyphicon-ok-sign', stateOff: 'glyphicon-ok-circle' },
+        { stateOn: 'glyphicon-star', stateOff: 'glyphicon-star-empty' },
+        { stateOn: 'glyphicon-heart', stateOff: 'glyphicon-ban-circle' },
+        { stateOn: 'glyphicon-heart' },
+        { stateOff: 'glyphicon-off' }
+    ];
 });// end of Places Controller
