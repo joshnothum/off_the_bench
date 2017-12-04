3f5e85770eb866a82f588476d48bf64134d565f4
Josh Nothum <joshnothum@gmail.com>
2017-12-03 23:08:07 -0600

    restarting modal

--- server/public/scripts/controllers/browse.controller.js
+++ server/public/scripts/controllers/browse.controller.js
@@ -1,3 +1,4 @@
+
 myApp.controller('BrowseController', function ($scope, $mdDialog, GameService, $http, $location, UserService, MapService, $uibModal) {
     GameService.browseGames();
     console.log('browsing is easy now!');
@@ -15,7 +16,7 @@ myApp.controller('BrowseController', function ($scope, $mdDialog, GameService, $
     };//end of getMoreGameInfo
     browse.joinGame = function (game_id) {
         console.log(game_id);
-        
+
         let playerJoin = {
             gameid: game_id
         };// end of playerJoin object
@@ -23,68 +24,12 @@ myApp.controller('BrowseController', function ($scope, $mdDialog, GameService, $
     };
 
 
-    browse.sendToMoment= function (time){
+    browse.sendToMoment = function (time) {
         moment(time).format('HH:MM');
 
         return time;
-        
+
     };
-    browse.animationsEnabled = true;
-
-    browse.open = function (size, parentSelector) {
-        var parentElem = parentSelector ?
-            angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
-        var modalInstance = $uibModal.open({
-            animation: $ctrl.animationsEnabled,
-            ariaLabelledBy: 'modal-title',
-            ariaDescribedBy: 'modal-body',
-            templateUrl: 'myModalContent.html',
-            controller: 'ModalInstanceCtrl',
-            controllerAs: '$ctrl',
-            size: size,
-            appendTo: parentElem,
-            resolve: {
-                items: function () {
-                    return $ctrl.items;
-                }
-            }
-        });
-
-        modalInstance.result.then(function (selectedItem) {
-            $ctrl.selected = selectedItem;
-        }, function () {
-            $log.info('Modal dismissed at: ' + new Date());
-        });
-    };
-
-    browse.openComponentModal = function () {
-        var modalInstance = $uibModal.open({
-            animation: $ctrl.animationsEnabled,
-            component: 'modalComponent',
-            resolve: {
-                items: function () {
-                    return $ctrl.items;
-                }
-            }
-        });
-
-        modalInstance.result.then(function (selectedItem) {
-            $ctrl.selected = selectedItem;
-        }, function () {
-            $log.info('modal-component dismissed at: ' + new Date());
-        });
-    };
-
-        $uibModal.open({
-            animation: $ctrl.animationsEnabled,
-            ariaLabelledBy: 'modal-title-top',
-            ariaDescribedBy: 'modal-body-top',
-            templateUrl: 'stackedModal.html',
-            size: 'sm',
-            controller: function ($scope) {
-                $scope.name = 'top';
-            }
-        });
 
 
 
