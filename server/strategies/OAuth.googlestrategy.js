21cd1166491c5a917652677e73bc5fe59647a9fe
Josh Nothum <joshnothum@gmail.com>
2017-12-11 15:00:05 -0600

    testing calendar

---
+++ server/strategies/OAuth.googlestrategy.js
@@ -0,0 +1,14 @@
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+require('dotenv').config();
+passport.use(new GoogleStrategy({
+    clientID: ClientID,
+    clientSecret: clientSecret,
+    callbackURL: "http://localhost:3000/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
