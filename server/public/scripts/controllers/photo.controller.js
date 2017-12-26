5edaf31c0d312dd11a59942c20b0c7b31f793040
Josh Nothum <joshnothum@gmail.com>
2017-12-26 09:14:24 -0600

    filestack is working

--- server/public/scripts/controllers/photo.controller.js
+++ server/public/scripts/controllers/photo.controller.js
@@ -1,19 +1,23 @@
 myApp.controller('PhotoController', function ($scope, $mdDialog, GameService, $http, $location, UserService, MapService) {
-
-
+    const apikey = 'AXGrpsfPWTs27zHOMSgc0z';
+    const client = filestack.init(apikey);
     let photo = this;
 
     photo.allLocations = MapService.allLocations;
     photo.moreInfo = MapService.getInfoForAdd;
     photo.location;
 
+function handleFilestack(info) {
+    console.log(info);
+    
+}
     
 
     
 console.log('photoController is here');
     var fsClient = filestack.init('AXGrpsfPWTs27zHOMSgc0z');
     photo.openPicker = function () {
-        fsClient.pick({
+        client.pick({
             fromSources: ["local_file_system", "imagesearch", "facebook", "instagram", "dropbox"]
         }).then(function (response) {
             // declare this function to handle response
